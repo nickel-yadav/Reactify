edc86f5a95d0e058fc58ee874bfa179025afad90
Nikhil Yadav <nickelyadav57@gmail.com>
2021-06-07 18:01:06 +0530

    added delete event handler passed down from app to task comp

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -22,11 +22,17 @@ function App() {
         day: 'Feb 5th at 2:30pm', 
         reminder: false, 
     },
-])
+]);
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
   return (
     <div className="container">
      <Header /> 
-     <Tasks tasks={tasks} />
+     {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} />
+     : 'No tasks to show'}
+     
     </div>
   );
 }
