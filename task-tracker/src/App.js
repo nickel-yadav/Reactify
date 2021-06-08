f72793a946f4c6eb9f1466393baf2439090f0ea8
Nikhil Yadav <nickelyadav57@gmail.com>
2021-06-08 19:28:08 +0530

    Created task highliter

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -27,13 +27,22 @@ function App() {
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
   }
+
+  // Toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => 
+      task.id === id ? {...task, reminder: 
+      !task.reminder } : task
+      )
+    );
+  }
+
   return (
     <div className="container">
-     <Header /> 
-     {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} />
-     : 'No tasks to show'}
-     
-    </div>
+     <Header />  
+     {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle ={toggleReminder} />
+     : 'No tasks to show'}     
+    </div> 
   );
 }
 
