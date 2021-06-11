fe05c77836d53c71395a403738c3b620d8932581
Nikhil Yadav <nickelyadav57@gmail.com>
2021-06-11 19:31:05 +0530

    Multiple products added to productlist

--- voting-app/src/ProductList.js
+++ voting-app/src/ProductList.js
@@ -1,49 +1,70 @@
 import React from 'react';
 import Product from './components/Product';
 
-const products = 
+const products = [
   {
     id: 1,
     title: 'Yellow Pail',
     description: 'On-demand sand castle construction expertise.',
     url: '#',
     votes: '57',
-  }
-//   {
-//     id: 2,
-//     title: 'Supermajority: The Fantasy Congress League',
-//     description: 'Earn points when your favorite politicians pass legislation.',
-//     votes: 44,
-//     url: '#',
-//   },
-//   {
-//     id: 3,
-//     title: 'Tinfoild: Tailored tinfoil hats',
-//     description: 'We already have your measurements and shipping address.',
-//     votes: 20,
-//     url: '#',
-//   },
-//   {
-//     id: 4,
-//     title: 'Haught or Naught',
-//     description: 'High-minded or absent-minded? You decide.',
-//     votes: 89,
-//     url: '#',
-//   },
-// ];
+    imageurl: 'https://images.unsplash.com/photo-1623162012953-5002981ac52d?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDQ0fGJvOGpRS1RhRTBZfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60',
+    avatarurl: 'https://image.freepik.com/free-vector/modern-people-avatar-casual-clothes-vector-cartoon-illustration-man-with-individual-face-hair-light-digital-frame-dark-blue-computer-picture-web-profile_107791-4246.jpg',
+  },
+  {
+    id: 2,
+    title: 'Supermajority: The Fantasy Congress League',
+    description: 'Earn points when your favorite politicians pass legislation.',
+    votes: 44,
+    url: '#',
+    imageurl: 'https://images.unsplash.com/photo-1575677753169-cff039a42afa?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDl8Ym84alFLVGFFMFl8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60',
+    avatarurl: 'https://image.freepik.com/free-vector/mysterious-mafia-man-smoking-cigarette_52683-34828.jpg',
+  },
+  {
+    id: 3,
+    title: 'Tinfoild: Tailored tinfoil hats',
+    description: 'We already have your measurements and shipping address.',
+    votes: 20,
+    url: '#',
+    imageurl: 'https://images.unsplash.com/photo-1623194279582-247ed47977b1?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE3fGJvOGpRS1RhRTBZfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60',
+    avatarurl: 'https://image.freepik.com/free-photo/medium-shot-happy-man-smiling_23-2148221808.jpg',
+  },
+  {
+    id: 4,
+    title: 'Haught or Naught',
+    description: 'High-minded or absent-minded? You decide.',
+    votes: 89,
+    url: '#',
+    imageurl: 'https://images.unsplash.com/photo-1585766765962-28aa4c7d719c?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDMzfGJvOGpRS1RhRTBZfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60',
+    avatarurl: 'https://image.freepik.com/free-vector/cute-girl-gaming-holding-joystick-cartoon-icon-illustration-people-technology-icon-concept-isolated-flat-cartoon-style_138676-2169.jpg',
+  },
+];
 
 const ProductList = (props) => {
-  return (
-    <div>
-      <Product 
-      id={products.id}
-      title={products.title}
-      description={products.description}
-      url={products.url}
-      vote={products.votes}
-      />
-    </div>
-  );
+
+  const  sorted_products = products.sort((a,b) => (
+    b.votes - a.votes
+  ));
+
+  const handleProductUpVote = (productId) => {
+    console.log(productId + 'was upvoted');
+  }
+
+  return sorted_products.map((product) => (
+          <div>
+            <Product 
+              key={'product-' + product.id}
+              id={product.id}
+              title={product.title}
+              description={product.description}
+              url={product.url}
+              vote={product.votes}
+              imageurl={product.imageurl}
+              avatarurl={product.avatarurl}
+              handleProductUpVote={handleProductUpVote(product.id)}
+            />
+          </div>
+  ));
 }
 
 
--- voting-app/src/components/Product.js
+++ voting-app/src/components/Product.js
@@ -5,16 +5,17 @@ const Product = (props) => {
     return (
         <Item.Group>
             <Item>
-            <Item.Image size="small" src="https://images.unsplash.com/photo-1622050644890-f4a3f807c2c6?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDI3fGJvOGpRS1RhRTBZfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60" />
+            <Item.Image size="tiny" src={props.imageurl} />
             
             <Item.Content>
                 <Item.Header as='a'>
-                <Icon name="angle up"></Icon><a>{props.vote}</a></Item.Header>
+                    <Icon name="angle up"></Icon><a>{props.vote}</a>
+                </Item.Header>
                 <Item.Description>
                     <h3>{props.title}</h3>
                     <p>{props.description}</p>
                     <span>Submitted by:</span>
-                    <Image avatar src="https://image.freepik.com/free-vector/modern-people-avatar-casual-clothes-vector-cartoon-illustration-man-with-individual-face-hair-light-digital-frame-dark-blue-computer-picture-web-profile_107791-4246.jpg"  wrapped />            
+                    <Image avatar src={props.avatarurl}  wrapped />            
                 </Item.Description>
             </Item.Content>
             </Item>  
