55151ea7caf857fde6d432f7f73115ba15d7bd9b
Nikhil Yadav <nickelyadav57@gmail.com>
2021-06-15 16:58:49 +0530

    Added states to button display

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 function App() {
+
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
         id: 1, 
@@ -24,6 +27,14 @@ function App() {
     },
 ]);
 
+  //Add a task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
   }
@@ -36,10 +47,13 @@ function App() {
       )
     );
   }
-
+  // This && operator works like a ternary if the condition was always true
   return (
     <div className="container">
-     <Header />  
+     <Header onAdd={() => 
+     setShowAddTask(!showAddTask)} showAdd={showAddTask}
+     />  
+     {showAddTask && <AddTask onAdd={addTask}/>} 
      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle ={toggleReminder} />
      : 'No tasks to show'}     
     </div> 
