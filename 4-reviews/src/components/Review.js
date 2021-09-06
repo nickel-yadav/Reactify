86de499837cf67b76abe96c4163caa73faba70fc
Nikhil <60028530+nickel-yadav@users.noreply.github.com>
2021-09-06 19:44:48 +0530

    :sparkles: adds buttons to change reviews

--- 4-reviews/src/components/Review.js
+++ 4-reviews/src/components/Review.js
@@ -1,10 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import people from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
 
 const Review = () => {
+    const [index, setIndex] = useState(0);
+    const { name, job, image, text } = people[index]; 
+
+    const checkNumber = (num) => {
+        if (num > people.length-1) {
+            return 0; 
+        }
+        if (num < 0) {
+            return people.length-1;
+        }
+        return num; 
+    }
+    
+    const prevPerson = () => {
+        setIndex((index) => {
+            let newIndex = index + 1;
+            return checkNumber(newIndex);
+        });
+    }
+
+    const nextPerson = () => {
+        setIndex((index) => {
+            let newIndex = index - 1; 
+            return checkNumber(newIndex);
+        })
+    }
+
+    const randomPerson = () => {
+        let randomNum = Math.floor(Math.random() * people.length);
+        if (randomNum === index) {
+            randomNum = index + 1; 
+        }
+        setIndex(checkNumber(randomNum));
+    }
+
     return (
-        <div>
-            <h2>Review Comp</h2>
-        </div>
+            <article className="review">
+                <div className="img-container">
+                    <img src={image} alt={name} className="person-img" />
+                    <span className="quote-icon">
+                        <FaQuoteRight />
+                    </span>
+                </div>
+                <h4 className="author">{name}</h4>
+                <p className="job">{job}</p>
+                <p className="info">{text}</p>
+                <div className="button-container">
+                    <button className="prev-btn" onClick={prevPerson}>
+                        <FaChevronLeft />
+                    </button>
+                    <button className="next-btn" onClick={nextPerson}>
+                        <FaChevronRight />
+                    </button>
+                </div>
+                <button className="random-btn" onClick={randomPerson}>surprise me</button>
+            </article>
     )   
 }
 
